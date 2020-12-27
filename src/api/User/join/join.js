78ba2422aa72b015f0fa9b8298a00f6c6effb6c8
badjfas <bjwkor@naver.com>
2020-12-27 23:01:00 +0900

    123123

--- src/api/User/join/join.js
+++ src/api/User/join/join.js
@@ -3,10 +3,10 @@ import { User } from "../../../sequelize";
 export default {
     Mutation: {
         join: async (_, args, {}) => {
-            const { email } = args;
+            const { studentId } = args;
             const hashPassword = crypto.createHash("sha512").update(args.password).digest("base64");
             try {
-                const emailCheck = await User.findOne({ where: { email: email } });
+                const emailCheck = await User.findOne({ where: { studentId: studentId } });
 
                 if (emailCheck === null) {
                     return await User.create({
